f5b0d329d770c13dd1eb457fcaa1212f5a1fcac8
mounika2demo <Nombre+76>
2023-09-14 22:55:55 +0530

    user implemented

--- application/backend/OnlineEventPlanner/src/com/demo/service/UserService.java
+++ application/backend/OnlineEventPlanner/src/com/demo/service/UserService.java
@@ -2,16 +2,75 @@ package com.demo.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
+import com.demo.exceptions.InvalidNumberOfPeopleException;
+import com.demo.exceptions.PlannedDateException;
 import com.demo.models.PlanRequest;
 import com.demo.models.Quotation;
 import com.demo.models.User;
 
 public interface UserService {
-    User findById(int userId) throws SQLException;
+	Scanner sc = new Scanner(System.in);
+
+	boolean validateUser(User u) throws SQLException;
+
+	User findById(int userId) throws SQLException;
+
 	boolean registerUser(User u) throws SQLException;
-    void updatePassword(User user, String newPassword) throws SQLException;
-    boolean addPlanRequest(PlanRequest pl) throws SQLException;
-    List<Quotation> viewQuotation(int uid) throws SQLException;
+
+	boolean updatePassword(int userid, String newPassword) throws SQLException;
+
+	boolean addPlanRequest(PlanRequest pl) throws SQLException, InvalidNumberOfPeopleException, PlannedDateException;
+
+	List<Quotation> viewQuotation(int uid) throws SQLException;
+
+	boolean updateQuotationStatus(int quotid, String status) throws SQLException;
+	
+	public void closeConnection() throws SQLException;
+
+	static PlanRequest getPlanRequestDetails() {
+		System.out.println("Enter requestid:");
+		int id = sc.nextInt();
+		System.out.println("Enter fromdate(yyyy-mm-dd):");
+		String fromDate = sc.next();
+		System.out.println("Enter todate(yyyy-mm-dd):");
+		String todate = sc.next();
+		System.out.println("Enter number of people attending:");
+		int np = sc.nextInt();
+		System.out.println("Enter userid:");
+		int uid = sc.nextInt();
+		System.out.println("Enter services:");
+		String services = sc.nextLine();
+		PlanRequest pl = new PlanRequest(id, np, uid, fromDate, todate, services);
+		return pl;
+	}
+
+	static User getUser() {
+		System.out.println("Enter userid:");
+		int uid = sc.nextInt();
+		System.out.println("Enter firstname:");
+		String fname = sc.next();
+		System.out.println("Enter Lastname:");
+		String lname = sc.next();
+		System.out.println("Enter username:");
+		String uname = sc.next();
+		System.out.println("Enter password:");
+		String pass = sc.next();
+		System.out.println("Enter doj(yyyy-mm-dd):");
+		String doj = sc.next();
+		System.out.println("Enter dob(yyyy-mm-dd):");
+		String dob = sc.next();
+		System.out.println("Enter department(Marketing,Sales,IT):");
+		String dept = sc.next();
+		System.out.println("Enter email:");
+		String email = sc.next();
+		System.out.println("Enter mobile:");
+		String mobile = sc.next();
+		System.out.println("Enter location:");
+		String location = sc.next();
+
+		return new User(uid, fname, lname, uname, pass, doj, dob, dept, email, mobile, location, "non-active");
+	}
 
 }
--- application/backend/OnlineEventPlanner/src/com/demo/service/UserServiceImpl.java
+++ application/backend/OnlineEventPlanner/src/com/demo/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.demo.service;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import com.demo.dao.UserDao;
 import com.demo.dao.impls.UserDaoImpl;
+import com.demo.exceptions.InvalidNumberOfPeopleException;
+import com.demo.exceptions.PlannedDateException;
 import com.demo.models.PlanRequest;
 import com.demo.models.Quotation;
 import com.demo.models.User;
@@ -13,8 +17,8 @@ public class UserServiceImpl implements UserService {
 	private static UserDao userDAO = new UserDaoImpl();
 
 	@Override
-	public void updatePassword(User user, String newPassword) throws SQLException {
-		userDAO.updatePasswd(user.getUserid(), newPassword);
+	public boolean updatePassword(int userid, String newPassword) throws SQLException {
+		return userDAO.updatePasswd(userid, newPassword);
 	}
 
 	@Override
@@ -28,12 +32,41 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public boolean addPlanRequest(PlanRequest pl) throws SQLException {
-		return userDAO.addPlanRequest(pl);
+	public boolean addPlanRequest(PlanRequest pl)
+			throws SQLException, InvalidNumberOfPeopleException, PlannedDateException {
+		if (pl.getNoOfPeopleAttending() > 30) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate plannedDate = LocalDate.parse(pl.getFromdate(), formatter);
+			LocalDate toDate = LocalDate.parse(pl.getTodate(), formatter);
+
+			if (plannedDate.isAfter(LocalDate.now()) && plannedDate.isBefore(toDate)) {
+				return userDAO.addPlanRequest(pl);
+			} else {
+				throw new PlannedDateException("Planned date should be greater than current date and less than toDate");
+			}
+		}
+		throw new InvalidNumberOfPeopleException("number of people attending should be greater than 30");
+
 	}
 
 	@Override
 	public List<Quotation> viewQuotation(int uid) throws SQLException {
 		return userDAO.viewquotation(uid);
+	}
+
+	@Override
+	public boolean validateUser(User u) throws SQLException {
+		return userDAO.validateUser(u);
+	}
+
+	@Override
+	public boolean updateQuotationStatus(int quotid, String status) throws SQLException {
+		return userDAO.updateQuotationStatus(quotid, status);
+	}
+
+	@Override
+	public void closeConnection() throws SQLException {
+		userDAO.closeConnection();
+
 	}
 }
