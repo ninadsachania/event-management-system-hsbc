cfc6b07d6f7dfc04be53012957b4a42d67176d3d
Ninad Sachania <ninad.sachania@gmail.com>
2023-09-14 22:05:07 +0530

    Implement the view layer functionality for admin

--- application/backend/OnlineEventPlanner/src/com/demo/main/Main.java
+++ application/backend/OnlineEventPlanner/src/com/demo/main/Main.java
@@ -1,17 +1,10 @@
 package com.demo.main;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
-import com.demo.dao.AdminDao;
-import com.demo.dao.UserDao;
-import com.demo.dao.VendorDao;
-import com.demo.dao.impls.AdminDaoImpl;
-import com.demo.dao.impls.UserDaoImpl;
-import com.demo.dao.impls.VendorDaoImpl;
-import com.demo.dao.util.DBUtil;
+import com.demo.models.Admin;
 import com.demo.models.User;
 import com.demo.models.Vendor;
 import com.demo.service.AdminService;
@@ -24,26 +17,44 @@ import com.demo.service.VendorServiceImpl;
 public class Main {
 	private static Scanner scanner = new Scanner(System.in);
 
+	static void printHeading(String msg) {
+		int length = msg.length();
+
+		for (int i = 0; i < length + 4; i += 1) {
+			System.out.print("-");
+		}
+		System.out.println();
+
+		System.out.println("  " + msg);
+
+		for (int i = 0; i < length + 4; i += 1) {
+			System.out.print("-");
+		}
+		System.out.println();
+
+	}
+
 	public static void main(String[] args) {
 		AdminService adminService = new AdminServiceImpl();
 		UserService userService = new UserServiceImpl();
 		VendorService vendorService = new VendorServiceImpl();
 
 		while (true) {
-			System.out.println("Main Menu:");
-			System.out.println("1. Admin");
-			System.out.println("2. Vendor");
-			System.out.println("3. User");
-			System.out.println("4. Exit");
-			System.out.print("Enter your choice: ");
+			try {
+				System.out.println("Main Menu:");
+				System.out.println("1. Admin");
+				System.out.println("2. Vendor");
+				System.out.println("3. User");
+				System.out.println("4. Exit");
+				System.out.print("Enter your choice: ");
 
-			int choice = scanner.nextInt();
-			scanner.nextLine();
+				int choice = Integer.parseInt(scanner.nextLine());
+//				scanner.nextLine();
 
-			switch (choice) {
-			case 1:
-				adminActions(adminService, userService, vendorService);
-				break;
+				switch (choice) {
+				case 1:
+					adminActions(adminService, userService, vendorService);
+					break;
 //                    case 2:
 //                        vendorActions(vendorService);
 //                        break;
@@ -55,56 +66,82 @@ public class Main {
 //                        DBUtil.closeConnection();
 //                        System.exit(0);
 //                        break;
-			default:
-				System.out.println("Invalid choice. Please try again.");
+				default:
+					System.out.println("Invalid choice. Please try again.");
+				}
+			} catch (SQLException e) {
+				System.err.println(e.getMessage());
 			}
 		}
 	}
 
 	private static void adminActions(AdminService adminService, UserService userService, VendorService vendorService)
 			throws SQLException {
+
+		boolean isLoggedIn = false;
+
+		while (!isLoggedIn) {
+			System.out.println("Please log in first...");
+			System.out.print("Username: ");
+			String username = scanner.nextLine().trim();
+			System.out.print("Password: ");
+			String password = scanner.nextLine().trim();
+			if (adminService.validateAdmin(new Admin(username, password))) {
+				System.out.println("Successfully logged in.");
+				break;
+			}
+
+			System.out.println("Wrong username/password. Try again");
+		}
 		// Implement admin actions here
-		System.out.println("Admin Actions:");
-		System.out.println("0. Login");
-		System.out.println("1. Add Vendor");
-		System.out.println("2. View Vendors");
-		System.out.println("3. View Users Registrations");
-		System.out.println("4. Update User Status");
-		System.out.println("5. Back to Main Menu");
-		System.out.print("Enter your choice: ");
-		
-		
-		
+		while (true) {
+			System.out.println("Admin Actions:");
+			System.out.println("1. Add Vendor");
+			System.out.println("2. View Vendors");
+			System.out.println("3. View Users Registrations");
+			System.out.println("4. Update User Status");
+			System.out.println("5. Back to Main Menu");
+			System.out.print("Enter your choice: ");
 
-		int choice = scanner.nextInt();
-		switch (choice) {
-		case 0:
-			break;
-		case 1:
-			
-			// Implement add vendor
-			break;
-		case 2:
-			List<Vendor> vendors = adminService.viewVendors();
+			int choice = Integer.parseInt(scanner.nextLine());
+			switch (choice) {
+			case 1:
+				// Add a new vendor
+				printHeading("Add a Vendor");
+				adminService.addVendor(VendorService.addNewVendor());
+				break;
+			case 2:
+				printHeading("View Vendor");
+				List<Vendor> vendors = adminService.viewVendors();
 
-			for (Vendor vendor : vendors) {
-				System.out.println(vendor);
+				for (Vendor vendor : vendors) {
+					System.out.println(vendor);
+				}
+				break;
+			case 3:
+				// Implement view users
+				printHeading("View Users");
+				List<User> users = adminService.viewUsers();
+				for (User user : users) {
+					System.out.println(user);
+				}
+				break;
+			case 4:
+				printHeading("Update User Status");
+				System.out.print("Enter user id: ");
+				scanner.nextLine();
+				int uid = Integer.parseInt(scanner.nextLine());
+
+				System.out.print("Enter the status: ");
+				String status = scanner.nextLine();
+
+				adminService.updateUserStatus(uid, status);
+				break;
+			case 5:
+				return;
+			default:
+				System.out.println("Invalid choice. Please try again.");
 			}
-			break;
-		case 3:
-			// Implement view users
-			List<User> users = adminService.viewUsers();
-			for (User user : users) {
-				System.out.println(user);
-			}
-			break;
-		case 4:
-			
-			break;
-		case 5:
-			return;
-		default:
-			System.out.println("Invalid choice. Please try again.");
 		}
 	}
 //
